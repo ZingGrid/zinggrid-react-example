817bf039cc4dce67bafb539f5a88b9ee154c5271
Vanna Luu <vluu@pb-mbp3c-a.ad.pintsd.com>
2018-03-14 11:47:04 -0700

    added readonly and datachange components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ReadOnly extends Component {
+  render() {
+    return (
+      <zing-grid data={this.props.datastore}></zing-grid>
+    )
+  }
+}
+
+class DataChange extends Component {
+  render() {
+    return ( 
+      <div>
+        <zing-grid editor></zing-grid>
+        <br />
+
+        <h3>Stored Data</h3>
+        <textarea name="ds" cols="50" rows="8"></textarea>
+        <br /><br />
+
+        <input type="checkbox" checked="{{pagerOn::change}}" />
+
+        <button id="cdBtn">Change Data</button>
+        <input id="cInput" type="text" placeholder="Caption" />
+        <button>Set Caption</button>
+      </div>   
+    )
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -99,6 +128,8 @@ class App extends Component {
           data={this.state.changedData}
         >
         </zing-grid>
+        <ReadOnly datastore="[[1,2,3], [4,5,6]]" />
+        <DataChange />
       </div>
     );
   }
